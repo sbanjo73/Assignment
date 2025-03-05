c805d1e0cf5928c3eb1c4d026b6096107655a72c
Oluwaseun Banjo <2375304@csl-student.wlv.ac.uk>
2025-03-05 17:15:38 +0000

    my third push

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -3,6 +3,7 @@
 
 use App\Controllers\News; // Add this line
 use App\Controllers\Pages;
+use App\Controllers\Ajax;
 
 use CodeIgniter\Router\RouteCollection;
 
@@ -11,4 +12,5 @@ $routes->get('news', [News::class, 'index']);           // Add this line
 $routes->get('news/(:segment)', [News::class, 'show']); // Add this line
 
 $routes->get('pages', [Pages::class, 'index']);
+$routes->get('ajaxnews/(:segment)', [Ajax::class, 'get']);
 $routes->get('(:segment)', [Pages::class, 'view']);
--- app/Views/news/index.php
+++ app/Views/news/index.php
@@ -1,5 +1,8 @@
 <h2><?= esc($title) ?></h2>
 
+<p id="ajaxArticle"></p>
+
+
 <?php if ($news_list !== []): ?>
 
     <?php foreach ($news_list as $news_item): ?>
@@ -10,6 +13,8 @@
             <?= esc($news_item['body']) ?>
         </div>
         <p><a href="/news/<?= esc($news_item['slug'], 'url') ?>">View article</a></p>
+  
+        <p><button onclick="getData('<?= esc($news_item['slug'], 'url') ?>')">View article via Ajax</button></p>
 
     <?php endforeach ?>
 
@@ -20,3 +25,24 @@
     <p>Unable to find any news for you.</p>
 
 <?php endif ?>
+
+<script>
+	function getData(slug) {
+		
+		// Fetch data
+		fetch('https://mi-linux.wlv.ac.uk/~in9352/ci4/public/ajax/get/' + slug)
+			
+		  // Convert response string to json object
+		  .then(response => response.json())
+		  .then(response => {
+
+			// Copy one element of response to our HTML paragraph
+			document.getElementById("ajaxArticle").innerHTML = response.title + ": " + response.body;
+		  })
+		  .catch(err => {
+			
+			// Display errors in console
+			console.log(err);
+		});
+	}
+</script>
